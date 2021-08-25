7f0090ebe105c493a5bad1ced73491c28454c7c3
Paul Marceau <paul@themarceaus.net>
2021-08-24 23:40:55 -0400

    Removed keys

--- server/config/keys.js
+++ server/config/keys.js
@@ -11,16 +11,3 @@ else {
 }
 
 
-
-
-/*
-module.exports = {
-    googleClientID: '574698205636-378u2tr60o22n8u3fk4u4edtgddueooq.apps.googleusercontent.com',
-    googleClientSecret: 'Vu_NaVU1EliQcwbpcbBBbsua',
-    facebookClientID: '593767521673359',
-    facebookClientSecret: 'd7c21ab3b00aae175c530b336cd9cded',
-    mongoURI: 'mongodb+srv://pmarceaujr:Mongodb4Me@feedbackcollector.gb4vq.mongodb.net/FeedbackCollector?retryWrites=true&w=majority',
-    cookieKey: 'thisIsMySecretCookieKey123',
-    mongodb_prod: 'mongodb+srv://pmarceaujr:Mongodb4Me@cluster0.sd51y.mongodb.net/Feedback-Prod?retryWrites=true&w=majority'
-}
-*/
